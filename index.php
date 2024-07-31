1499ced2afd8dd1759138f4aad3360862ec9cff8
vicky5571 <vickygp5571@gmail.com>
2024-07-31 23:41:51 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium numquam ipsa in quo repellat recusandae voluptatibus, omnis vel quas, ratione magni fuga id assumenda fugiat labore tenetur, animi amet adipisci.</p>
+    <ul>
+        <li>asd</li>
+    </ul>
 </body>
 </html>
