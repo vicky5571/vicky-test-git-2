6cefb1cb0942d9a898865731532d3da013266345
vicky5571 <vickygp5571@gmail.com>
2024-07-31 23:05:07 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium numquam ipsa in quo repellat recusandae voluptatibus, omnis vel quas, ratione magni fuga id assumenda fugiat labore tenetur, animi amet adipisci.</p>
 </body>
 </html>
